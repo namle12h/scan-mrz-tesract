46f6959d7d5d5b9b1cfa320859adb232478d35a3
Namle12h <namle2001vlogs@gmail.com>
2026-03-11 11:54:43 +0700

    update v22

--- build.sh
+++ build.sh
@@ -2,3 +2,5 @@
 
 apt-get update
 apt-get install -y tesseract-ocr
+
+pip install -r requirements.txt
--- scan_tesract.py
+++ scan_tesract.py
@@ -27,9 +27,8 @@ tesseract_path = shutil.which("tesseract")
 
 if tesseract_path:
     pytesseract.pytesseract.tesseract_cmd = tesseract_path
-else:
+elif os.name == "nt":
     pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-
 
 
 os.makedirs("debug", exist_ok=True)
